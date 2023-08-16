119fb58752648e8dc51745d73b13a4685f0d0c7f
JRoberto <kanon939@hotmail.com>
2023-08-15 22:22:52 -0700

    update

---
+++ app.js
@@ -0,0 +1,6 @@
+const socket = io();
+
+const chatMessages = document.getElementById('chat-messages');
+const messageInput = document.getElementById('message-input');
+const sendButton = document.getElementById('send-button');
+
